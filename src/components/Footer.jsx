dc6f76c9103cdbc008fb3f88ec38baabd45ce467
Rendiero <workspace.rendy@gmail.com>
2025-06-24 12:00:32 +0700

    revisi footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,10 +42,13 @@ const Footer = () => {
 
                 <div className="flex flex-col xl:flex-row justify-between items-center gap-4 w-full">
                     <div className="flex flex-row justify-center items-center gap-2">
-                        <img src={IconWA} alt="icon-wa" className="w-6"/>
-                        <img src={IconMail} alt="icon-wa" className="w-7"/>
-                        <img src={IconIG} alt="icon-ig" className="w-6"/>
-                        <img src={IconFB} alt="icon-fb" className="w-6"/>
+                        <a href="https://wa.me/6289539336946?text=Halo%20Duasisi%20Sejahtera,%20Saya%20ingin%20konsultasi%20lebih%20lanjut">
+                            <img src={IconWA} alt="icon-wa" className="w-6" />
+                        </a>
+
+                        <a href="https://www.instagram.com/duasisisejahtera">
+                            <img src={IconIG} alt="icon-ig" className="w-6"/>
+                        </a>
                     </div>
 
                     <p className="text-white text-sm font-reguler text-center xl:text-right">
